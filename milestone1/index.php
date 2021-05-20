43e3bd4c14ed5efe9c497c34dc01ae8cfda610bb
Robert-Stefan <robertboss05@hotmail.it>
2021-05-20 17:31:21 +0200

    finish php css work in progress

---
+++ milestone1/index.php
@@ -0,0 +1,37 @@
+<?php // collegamento con database.php
+    require_once __DIR__ . './database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Spotify</title>
+</head>
+<body>
+
+    <!--HEADER-->
+    <header>
+        <section class="container">
+            <img src="./img/logo.png" alt="logo">           
+        </section>
+    </header>
+
+    <!--MAIN CONTENT-->
+    <main>
+        <section class="content">
+            <?php foreach ($database as $music) { ?>
+                <div class="dischi">
+                    <img src=" <?php echo $music['poster']; ?> " alt="disco">
+                    <h3> <?php echo $music['title']; ?> </h3>
+                    <p class="titles"> <?php echo $music['author']; ?> </p>
+                    <p> <?php echo $music['year']; ?> </p>
+                    <p class="titles"> <?php echo $music['genre']; ?> </p>
+                </div>
+           <?php } ?>
+        </section>
+    </main>
+</body>
+</html>
